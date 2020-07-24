4060ed16e040129b18e18d7dbdf48c41d12f16f4
Jagrati Pahlajwani <pjag2403@gmail.com>
2020-07-24 19:34:44 +0530

    New: Main Task Page
    Signed-off-by: Jagrati Pahlajwani <pjag2403@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-import '../assets/css/style.css';
+import '../assets/css/main.css';
+
+var addTag = document.querySelector(".add-tag-btn");
+var tagBox = document.querySelector(".add-tag-popup");
+
+// addTag.onclick = function ()
+// {
+//     tagBox.style.display= "block";
+// }
+
+// window.onclick = function(event) 
+// {
+//     if(event.target == tagBox)
+//     {
+//         tagBox.style.display = "none";
+//     }
+// }
+
+
+function popupBox()
+{
+    tagBox.style.display= "block";
+}
+
+// if(addTag){
+//     addTag.addEventListener('click', popupBox, false);
+//   }
+
+addTag.addEventListener('click',popupBox);
+
+
+
